7d7ba9febbfc9c81e262fcbfe9ad7d89a8adeee7
Sarath <vssarathc04@gmail.com>
2023-11-22 20:41:01 +0530

    + linter for python, go

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,4 +1,5 @@
 local null_ls = require "null-ls"
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local b = null_ls.builtins
 
@@ -13,9 +14,36 @@ local sources = {
 
   -- cpp
   b.formatting.clang_format,
+
+  -- python
+  b.formatting.black,
+  -- b.diagnostics.mypy,
+  b.diagnostics.ruff,
+  -- b.diagnostics.pylint
+
+  -- go
+  b.formatting.gofumpt,
+  b.formatting.goimports_reviser,
+  b.formatting.golines,
 }
 
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
+        group = augroup,
+        buffer = bufnr,
+      }
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
 }
