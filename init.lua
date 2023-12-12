f67a82aad3ee9645f1799621de96bc7e7dd03f60
Sarath <vssarathc04@gmail.com>
2023-12-12 08:30:47 +0530

    wrap = false

--- init.lua
+++ init.lua
@@ -6,6 +6,6 @@
 --   command = "tabdo wincmd =",
 -- })
 vim.wo.relativenumber = true
-vim.o.wrap = false;
+vim.o.wrap = false
 -- vim.o.shell = vim.fn.executable('pwsh') and 'pwsh' or 'powershell'
 -- vim.g.copilot_assume_mapped = true
