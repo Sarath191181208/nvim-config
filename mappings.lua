3cab94dc690068d9b6f8510dfe21e594008e1c2c
Sarath <vssarathc04@gmail.com>
2023-12-13 00:12:40 +0530

    some remaps for easier use

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,12 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>d"] = { '"_d', "delete without yanking" },
+    ["<leader>p"] = { '[["_dP]]', "paste the text whithout copying " },
+  },
+  i = {
+    -- making ctrl+backspace = ctrl+w
+    ["<C-h>"] = { "<C-w>", "Delete a word" },
   },
   v = {
     [">"] = { ">gv", "indent" },
